757edc1b3a1a02107f76e53d1f7c2ac4708e6c79
Jake Keller <jakeller3630@gmail.com>
2024-12-01 18:57:30 -0500

    Much improved day 1

--- day_01/01.hs
+++ day_01/01.hs
@@ -1,44 +1,27 @@
 import System.Environment (getArgs)
 import System.IO
 import Data.List (sort, foldl')
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
+import Data.Bifunctor (bimap)
 
-parseLine :: String -> ([Int], [Int]) -> ([Int], [Int])
-parseLine cs (ls, rs) = (l:ls, r:rs)
+parseLine :: String -> (Int, Int)
+parseLine cs = (l, r)
     where ws = words cs
           l = read $ head ws
           r = read $ last ws
 
-parseLists :: [String] -> ([Int], [Int])
-parseLists = foldr parseLine ([], [])
+folder (rs, acc) l = let cnt = length $ takeWhile (l ==) $ dropWhile (l >) rs
+                         rs' = dropWhile (l >) rs
+                     in  (rs', acc + l * cnt)
 
--- TODO: use Arrows???
-sortLists :: ([Int], [Int]) -> ([Int], [Int])
-sortLists (ls, rs) = (sort ls, sort rs)
-
-distances :: ([Int], [Int]) -> Int
-distances (ls, rs) = sum $ zipWith (\x y -> abs $ x - y) ls rs
-
--- TODO: yes this is inefficient
-mapper (rs', m) l = let cnt = length $ takeWhile (l ==) $ dropWhile (l >) rs'
-                        m' = Map.insertWith (+) l cnt m
-                        rs'' = dropWhile (l >) rs'
-                    in  (rs'', m')
-
-buildMap :: ([Int], [Int]) -> Map Int Int
-buildMap (ls, rs) = snd $ foldl' mapper (rs, Map.empty) ls
-
-crunchMap :: Map Int Int -> Int
-crunchMap = Map.foldlWithKey' (\acc k v -> acc + k * v) 0
+similarity :: ([Int], [Int]) -> Int
+similarity (ls, rs) = snd $ foldl' folder (rs, 0) ls
 
 main = do
     filename_list <- getArgs
     let filename = head filename_list
     contents <- readFile filename
-    let sorted = sortLists $ parseLists $ lines contents
+    let sorted = bimap sort sort $ unzip $ map parseLine $ lines contents
     putStrLn "Part 1:"
-    print $ distances $ sorted
+    print $ sum $ uncurry (zipWith (\x y -> abs $ x - y)) sorted
     putStrLn "Part 2:"
-    -- TODO: combine parsing
-    print $ crunchMap $ buildMap $ sorted
+    print $ similarity sorted
