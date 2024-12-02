bb9b55ead8840ac14bb8b32224efc0655284b641
Jake Keller <jakeller3630@gmail.com>
2024-12-02 17:37:18 -0500

    Add working Day 2 p2

--- day_02/02.hs
+++ day_02/02.hs
@@ -15,6 +15,28 @@ reportSafe cs =
         up = head ls < (ls !! 1)
     in  and $ zipWith (levelSafe up) ls $ tail ls
 
+-- Part 2
+
+parseList :: String -> [Int]
+parseList = map read . words
+
+listSafe :: [Int] -> Bool
+listSafe ls =
+    let up = head ls < (ls !! 1)
+        safes = zipWith (levelSafe up) ls $ tail ls
+        badCount = foldl' (\acc x -> if x then acc + 1 else acc) 0 safes
+        good = map snd $ takeWhile fst $ zip safes ls
+        badIdx = length good
+        firstDamp = listSafe' $ good ++ drop (badIdx + 1) ls
+        secondDamp = listSafe' $ good ++ [ls !! badIdx] ++ drop (badIdx + 2) ls
+        dropFirst = listSafe' $ drop 1 ls
+    in  and safes || firstDamp || secondDamp || dropFirst
+
+listSafe' :: [Int] -> Bool
+listSafe' ls =
+    let up = head ls < (ls !! 1)
+    in  and $ zipWith (levelSafe up) ls $ tail ls
+
 main = do
     filename_list <- getArgs
     let filename = head filename_list
@@ -22,3 +44,4 @@ main = do
     putStrLn "Part 1:"
     print $ foldl' (flip ((+) . fromEnum)) 0 $ map reportSafe $ lines contents
     putStrLn "Part 2:"
+    print $ foldl' (flip ((+) . fromEnum)) 0 $ map (listSafe . parseList) $ lines contents
