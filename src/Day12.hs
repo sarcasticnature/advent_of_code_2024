34cf9561573f38d82e93e1ccc1c512a971efdf2f
Jake Keller <jakeller3630@gmail.com>
2025-11-13 18:48:19 -0500

    Finish day 12 p2

--- src/Day12.hs
+++ src/Day12.hs
@@ -3,7 +3,7 @@ module Day12 (day12) where
 import qualified Data.HashMap.Strict as HM
 import Data.IntSet (IntSet)
 import qualified Data.IntSet as IntSet
-import Data.List (foldl')
+import Data.List (foldl', sortBy)
 import qualified Data.Vector.Unboxed as VU
 
 -- Part 1
@@ -37,11 +37,11 @@ segment w h cs = fst $ VU.ifoldl' run (HM.empty, IntSet.empty) cs
 price :: Int -> Int -> IntSet -> Int
 price w h is =
   let area = IntSet.size is
-      sides i =
+      freeSides i =
         let nn = neighbors w h i
             f acc n = if IntSet.member n is then acc - 1 else acc
          in foldl' f 4 nn
-      perim = IntSet.foldl' (\acc i -> acc + sides i) 0 is
+      perim = IntSet.foldl' (\acc i -> acc + freeSides i) 0 is
    in area * perim
 
 segmentPrices :: Int -> Int -> HM.HashMap Int IntSet -> [Int]
@@ -51,12 +51,60 @@ segmentPrices w h = HM.foldl' f []
 
 -- Part 2
 
+verticalSides :: Int -> Int -> VU.Vector Char -> Int -> (Bool, Bool)
+verticalSides w h cs i =
+  let char = cs VU.! i
+      north = i - w < 0 || char /= (cs VU.! (i - w))
+      south = i + w >= w * h || char /= (cs VU.! (i + w))
+   in (north, south)
+
+horizontalSides :: Int -> Int -> VU.Vector Char -> Int -> (Bool, Bool)
+horizontalSides w h cs i =
+  let char = cs VU.! i
+      west = i - 1 < 0 || (i - 1) `mod` w == w - 1 || char /= (cs VU.! (i - 1))
+      east = i + 1 >= w * h || (i + 1) `mod` w == 0 || char /= (cs VU.! (i + 1))
+   in (west, east)
+
+sides :: Int -> Int -> VU.Vector Char -> IntSet -> Int
+sides w h cs is =
+  let iList = IntSet.elems is
+      go_h (tb, acc) [] = (tb, acc)
+      go_h ((t, b), acc) (i : il) =
+        let (t', b') = verticalSides w h cs i
+            topSide = fromEnum $ t' && not t
+            bottomSide = fromEnum $ b' && not b
+            hasNext = (i + 1) `mod` w /= 0 && IntSet.member (i + 1) is
+            (t'', b'') = if hasNext then (t', b') else (False, False)
+         in go_h ((t'', b''), acc + topSide + bottomSide) il
+      scan_h = snd $ go_h ((False, False), 0) iList
+      vertSort i_a i_b
+        | mod_a < mod_b = LT
+        | mod_a > mod_b = GT
+        | i_a < i_b = LT
+        | i_a > i_b = GT
+        | otherwise = EQ
+        where
+          mod_a = i_a `mod` w
+          mod_b = i_b `mod` w
+      go_v (lr, acc) [] = (lr, acc)
+      go_v ((l, r), acc) (i : il) =
+        let (l', r') = horizontalSides w h cs i
+            leftSide = fromEnum $ l' && not l
+            rightSide = fromEnum $ r' && not r
+            hasNext = (i + w) < w * h && IntSet.member (i + w) is
+            (l'', r'') = if hasNext then (l', r') else (False, False)
+         in go_v ((l'', r''), acc + leftSide + rightSide) il
+      scan_v = snd $ go_v ((False, False), 0) $ sortBy vertSort iList
+   in IntSet.size is * (scan_h + scan_v)
+
 day12 :: String -> IO ()
 day12 filename = do
   contents <- readFile filename
   let height = length $ lines contents
   let width = length $ head $ lines contents
   putStrLn "\nPart 1:"
-  let hm = segment width height $ parseMap contents
+  let gardenMap = parseMap contents
+  let hm = segment width height gardenMap
   print $ sum $ segmentPrices width height hm
   putStrLn "\nPart 2:"
+  print $ sum $ map (sides width height gardenMap . snd) $ HM.toList hm
